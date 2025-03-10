6cbc73c001842289cb6baf6c4aa6af7c82395e8f
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2025-03-10 10:23:17 +0800

    chore: added script to import and export assets easily

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import LogoDark from "./logos/logo-dark.png"
+import LogoLight from "./logos/logo-light.png"
+import LogoBannerLight from "./logos/light.png"
+import LogoBannerDark from "./logos/dark.png"
+
+export { LogoDark, LogoLight, LogoBannerLight, LogoBannerDark }
